1c3f035a7b03d9c1ae8cf1700b6bd8dc7dd65a85
Erwin Salas <erwin.salas@tsi.com>
2023-10-08 00:16:48 -0600

    Add docs, goreleaser, and GH actions

--- main.go
+++ main.go
@@ -33,26 +33,3 @@ func main() {
 
 	tf5server.Serve(providerName, muxer.ProviderServer, opts...)
 }
-
-// func containerCliTest() {
-// 	cli, err := client.NewClientWithOpts(client.FromEnv)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-// 	fmt.Println(containers)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
-// 	fmt.Println(images)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	for _, container := range containers {
-// 		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
-// 	}
-// }
